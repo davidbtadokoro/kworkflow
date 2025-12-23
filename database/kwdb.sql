d64b015ee2225b44157f3a7d7699313c770ec880
JGBSouza <joaosouzaaa12@usp.br>
2025-12-22 22:03:36 -0300

    fix: remove database unused attributes
    Signed-off-by: JGBSouza <joaosouzaaa12@usp.br>

--- database/kwdb.sql
+++ database/kwdb.sql
@@ -50,15 +50,14 @@ CREATE TABLE IF NOT EXISTS "event" (
 -- to the executed commands that are saved and the pomodoro sessions created
 CREATE TABLE IF NOT EXISTS "patch" (
   "id" INTEGER,
-  "created_at" TEXT DEFAULT (datetime('now','localtime')),
-  "status" VARCHAR(50) DEFAULT ('SENT') NOT NULL,
-  "title" TEXT NOT NULL, 
+  "title" TEXT NOT NULL,
   "author_email" TEXT,
-  "contribution_id" INTEGER NOT NULL,
+  "status" VARCHAR(50) DEFAULT ('SENT') NOT NULL,
   "commit_hash" TEXT,
+  "created_at" TEXT DEFAULT (datetime('now','localtime')),
+  "contribution_id" INTEGER NOT NULL,
   CHECK ("status" IN ('SENT', 'APPROVED', 'MERGED', 'REVIEWED', 'REJECTED')),
   PRIMARY KEY("id"),
-  FOREIGN KEY ("last_patch_id") REFERENCES "patch"("id") ON DELETE CASCADE,
   FOREIGN KEY ("contribution_id") REFERENCES "contribution"("id") ON DELETE CASCADE
 );
 
@@ -80,6 +79,7 @@ CREATE TABLE IF NOT EXISTS "submission" (
   "id" INTEGER,
   "contribution_id" INTEGER NOT NULL,
   "send_by" TEXT NOT NULL,
+  "created_at" TEXT DEFAULT (datetime('now','localtime')),
   PRIMARY KEY("id"),
   FOREIGN KEY ("contribution_id") REFERENCES "contribution"("id") ON DELETE CASCADE
 );
@@ -87,8 +87,8 @@ CREATE TABLE IF NOT EXISTS "submission" (
 CREATE TABLE IF NOT EXISTS "patch_submission" (
   "patch_id" INTEGER NOT NULL,
   "submission_id" INTEGER NOT NULL,
-  "created_at" TEXT DEFAULT (datetime('now','localtime')),
   "message_id" TEXT NOT NULL,
+  "created_at" TEXT DEFAULT (datetime('now','localtime')),
   PRIMARY KEY ("patch_id", "submission_id", "message_id"),
   FOREIGN KEY ("submission_id") REFERENCES "submission"("id") ON DELETE CASCADE,
   FOREIGN KEY ("patch_id") REFERENCES "patch"("id") ON DELETE CASCADE
@@ -96,13 +96,13 @@ CREATE TABLE IF NOT EXISTS "patch_submission" (
 
 CREATE TABLE IF NOT EXISTS "contribution" (
   "id" INTEGER,
+  "title" TEXT NOT NULL,
   "created_at" TEXT DEFAULT (datetime('now','localtime')),
-  "status" VARCHAR(50) DEFAULT ('SENT') NOT NULL,
-  "title" TEXT NOT NULL UNIQUE,
   "last_interaction_at" TEXT DEFAULT (datetime('now','localtime')),
-  "active" INTEGER NOT NULL CHECK ("active" IN (0, 1)) DEFAULT 1,
+  "status" VARCHAR(50) DEFAULT ('SENT') NOT NULL,
   "author_email" TEXT NOT NULL,
   "repository_id" INTEGER,
+  UNIQUE("title", "author_email")
   PRIMARY KEY("id"),
   FOREIGN KEY ("repository_id") REFERENCES "repository"("id") ON DELETE CASCADE
 );
@@ -111,18 +111,27 @@ CREATE TABLE IF NOT EXISTS "repository" (
   "id" INTEGER,
   "created_at" TEXT DEFAULT (datetime('now','localtime')),
   "name" TEXT NOT NULL,
-  "origin_url" TEXT NOT NULL,
-  "branch_name" TEXT NOT NULL,
+  "origin_url" TEXT NOT NULL UNIQUE,
   PRIMARY KEY("id")
-  UNIQUE("origin_url", "branch_name")
 );
 
 CREATE TABLE IF NOT EXISTS "repository_maintainer" (
   "id" INTEGER,
   "repository_id" INTEGER NOT NULL,
   "contact_id" INTEGER NOT NULL,
-  PRIMARY KEY ("repository_id", "contact_id"),
-  FOREIGN KEY ("repository_id") REFERENCES "repository"("id") ON DELETE CASCADE
+  PRIMARY KEY("id"),
+  UNIQUE ("repository_id", "contact_id"),
+  FOREIGN KEY ("repository_id") REFERENCES "repository"("id") ON DELETE CASCADE,
+  FOREIGN KEY ("contact_id") REFERENCES "email_contact"("id") ON DELETE CASCADE
+);
+
+-- Table containing the kw email contacts infos
+CREATE TABLE IF NOT EXISTS "email_contact" (
+    "id" INTEGER NOT NULL UNIQUE,
+    "name" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(100) NOT NULL UNIQUE,
+    "created_at" TEXT DEFAULT (date('now', 'localtime')),
+    PRIMARY KEY("id")
 );
 
 -- This is the relationship between an "event" that executes a given
